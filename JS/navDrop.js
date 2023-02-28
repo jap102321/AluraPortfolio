c8803abdc797622af1d53a29250975258b881318
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-02-28 09:58:52 -0500

    Adding header/responsive navbar

---
+++ JS/navDrop.js
@@ -0,0 +1,6 @@
+const hamburguer = document.querySelector("[data-hamburguer-nav]");
+const navList = document.querySelector("[data-nav-list]")
+
+hamburguer.addEventListener("click", ()=>{
+    navList.classList.toggle("dropdown")
+})
