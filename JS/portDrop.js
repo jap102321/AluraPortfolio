9e204a7794682734b070fb1bf334eb5aa933c601
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-03-10 23:57:55 -0500

    Projects part ended(Need some debugging and responsive part  but it is functional)

--- JS/portDrop.js
+++ JS/portDrop.js
@@ -3,7 +3,23 @@ const dropdownInfo = document.querySelectorAll("[data-main]");
 dropdownInfo.forEach((el) => {
   const select = el.querySelector("[data-moreInfo]");
   const menu = el.querySelector("[data-projectPort]");
+  const unselect = el.querySelector("[data-lessInfo]");
   select.addEventListener("click", () => {
     menu.classList.toggle("dropped");
+    menu.classList.toggle("undrop");
+    select.classList.toggle("hide");
+    unselect.classList.toggle("hide");
+    if (
+      select.classList.contains("hide") &&
+      unselect.classList.contains("hide")
+    ) {
+      select.classList.remove("hide");
+    }
+    unselect.addEventListener("click", () => {
+      menu.classList.remove("dropped");
+      menu.classList.toggle("undrop");
+      select.classList.toggle("hide");
+      unselect.classList.toggle("hide");
+    });
   });
 });
